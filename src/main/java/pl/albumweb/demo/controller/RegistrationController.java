c91e47eb4d6aacfdb6304765ba3acc32bd2aeb41
Arkadiusz-K <arkadiusz.kar96@gmail.com>
2020-07-20 22:57:51 +0200

    baza danych i rejestracja

--- src/main/java/pl/albumweb/demo/controller/RegistrationController.java
+++ src/main/java/pl/albumweb/demo/controller/RegistrationController.java
@@ -1,5 +1,9 @@
 package pl.albumweb.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +11,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.albumweb.demo.model.UserDTO;
+import pl.albumweb.demo.repository.UserDTORepo;
 
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
+    private UserDTORepo userDTORepo;
+    public RegistrationController(UserDTORepo userDTORepo){
+        this.userDTORepo = userDTORepo;
+    }
+
     @GetMapping
     public String registration(Model model){
         UserDTO userDTO = new UserDTO();
@@ -22,6 +32,8 @@ public class RegistrationController {
     public String registration2(@ModelAttribute("userDTO") UserDTO userDTO){
         System.out.println(userDTO.getName());
         System.out.println(userDTO.getId());
+        userDTORepo.save(userDTO);
         return "home";
     }
+
 }
